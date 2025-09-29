ca565125e4840f366957c1ed77a9a34f108f1649
fogazza <vfogacacintra@gmail.com>
2025-09-29 09:06:35 -0300

    29-09-25 / feat: Implementando Novas Funções

--- alarmeEsp.ino
+++ alarmeEsp.ino
@@ -1,20 +1,18 @@
 #define pinoBuzzer 27
 #define pinoBotao 26
-#define pinoLed 13
 #define pinoTrigger 12
 #define pinoEcho 14
 
-#define pinoLedAmarelo 25
-#define pinoLedVerde 32
+#define pinoLedVermelho 13
+#define pinoLedAmarelo 22
+#define pinoLedVerde 23
 
 #include "AdafruitIO_WiFi.h"
 #include "NewPing.h"
 
-// // Configuração do WiFi
 #define WIFI_SSID "fogazzaa"
 #define WIFI_PASS "fogazzaa"
 
-// // Autenticação AdafuritIO
 #define IO_USERNAME "fogazza"
 #define IO_KEY "aio_Ymaa42WF9j7LgA6Lq1DBEqIzdOB5"
 
@@ -22,6 +20,9 @@ bool alarmeAtivo = false;
 unsigned int distancia = 0;
 int limiteDistancia = 15;
 int maxDistancia = 100;
+
+unsigned int ultimaDistanciaEnviada = 0;
+bool isInitialSync = true;
 
 AdafruitIO_WiFi io(IO_USERNAME, IO_KEY, WIFI_SSID, WIFI_PASS);
 
@@ -32,7 +33,10 @@ AdafruitIO_Feed *sensorultrassonico = io.feed("sensorultrassonico");
 
 void setup() {
 
-  pinMode(pinoLed, OUTPUT);
+  pinMode(pinoLedVermelho, OUTPUT);
+  pinMode(pinoLedVerde, OUTPUT);
+  pinMode(pinoLedAmarelo, OUTPUT);
+
   pinMode(pinoBuzzer, OUTPUT);
   pinMode(pinoBotao, INPUT);
   Serial.begin(115200);
@@ -52,12 +56,8 @@ void setup() {
     delay(500);
   }
 
-  // Mostra status da conexão
-
   Serial.println();
   Serial.println(io.statusText());
-
-  // Configuração do Callback, quando o feed recebe(atualizar) um valor
 
   botaoalarme->onMessage(handleAlarme);
 
@@ -65,11 +65,13 @@ void setup() {
   botaoalarme->get();
 
   delay(1000);
+
+  isInitialSync = false;
 }
 
 void loop() {
 
-  io.run();  // Manter a conexão com a AdrafuitIO Ativa
+  io.run();
 
   if (digitalRead(pinoBotao) == 1) {
     delay(200);
@@ -92,7 +94,8 @@ void loop() {
 
   delay(3000);
 
-  if (distancia != 0) {
+  if (distancia != ultimaDistanciaEnviada) {
     sensorultrassonico->save(distancia);
+    ultimaDistanciaEnviada = distancia;
   }
 }
--- handleAlarme.ino
+++ handleAlarme.ino
@@ -1,30 +1,47 @@
-
 void handleAlarme(AdafruitIO_Data *data) {
 
   String valor = data->toString();
 
-  Serial.print(F("Valor Recebido do Feed: "));
-  Serial.println(valor);
+  if (!isInitialSync) {
+    Serial.print(F("Valor Recebido do Feed: "));
+    Serial.println(valor);
+  }
 
   if (valor == "true") {
     alarmeAtivo = true;
-    Serial.println("Alarme ARMADO no/pelo Adafruit/Mobile");
+    if (!isInitialSync) {
+      Serial.println("Alarme ARMADO no/pelo Adafruit/Mobile");
+    }
     digitalWrite(pinoLedAmarelo, HIGH);
     digitalWrite(pinoLedVerde, LOW);
   } else {
     alarmeAtivo = false;
-    Serial.println("Alarme DESARMADO no/pelo Adafruit/Mobile");
+    if (!isInitialSync) {
+      Serial.println("Alarme DESARMADO no/pelo Adafruit/Mobile");
+    }
     digitalWrite(pinoLedAmarelo, LOW);
     digitalWrite(pinoLedVerde, HIGH);
   }
 }
 
 void ativarAlerta() {
+
+  digitalWrite(pinoLedAmarelo, HIGH);
+  digitalWrite(pinoLedVerde, LOW);
+
   digitalWrite(pinoBuzzer, HIGH);
-  digitalWrite(pinoLed, HIGH);
+  digitalWrite(pinoLedVermelho, HIGH);
+
+  delay(750);
+
+  digitalWrite(pinoBuzzer, LOW);
+  digitalWrite(pinoLedVermelho, LOW);
 }
 
 void desativarAlerta() {
   digitalWrite(pinoBuzzer, LOW);
-  digitalWrite(pinoLed, LOW);
+  digitalWrite(pinoLedVermelho, LOW);
+
+  digitalWrite(pinoLedAmarelo, LOW);
+  digitalWrite(pinoLedVerde, HIGH);
 }
